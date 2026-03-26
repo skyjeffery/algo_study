22c30eef39652a3b2a3a2008bb3d10ae595e71ef
skyjeffery <REDMI200520@outlook.com>
2026-03-26 22:35:24 +0800

    Add files via upload

---
+++ usehotel.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "hotel.h"
+
+int main(void)
+{
+	int code, nights;
+	double hotel_rate;
+	
+	while ((code = menu()) != 5)
+	{
+		switch (code)
+		{
+			case 1: hotel_rate = HOTEL1;
+					break;
+			case 2: hotel_rate = HOTEL2;
+					break;
+			case 3: hotel_rate = HOTEL3;
+				break;
+			case 4: hotel_rate = HOTEL4;
+				break;
+			default: hotel_rate = 0.0;
+				printf("o!");
+		}
+		nights = get_nights();
+		show_price(hotel_rate, nights);
+	}
+	printf("Have a good time!\n");
+	return 0;
+}
